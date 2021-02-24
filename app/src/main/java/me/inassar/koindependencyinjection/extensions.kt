056b7cdc1365480c788338a3bb7df74187ad5ce9
ranger163 <ahmed.nassar163@gmail.com>
2021-02-24 15:36:45 +0300

    Adding snackBar extension to extensions.kt file

--- app/src/main/java/me/inassar/koindependencyinjection/extensions.kt
+++ app/src/main/java/me/inassar/koindependencyinjection/extensions.kt
@@ -1,1 +1,8 @@
 package me.inassar.koindependencyinjection
+
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+
+fun View.snack(message: String) {
+    Snackbar.make(this, message, Snackbar.LENGTH_SHORT).show()
+}
