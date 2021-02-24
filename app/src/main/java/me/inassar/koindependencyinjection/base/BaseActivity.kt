e73914c8f768e2f5969715320b9a962da22f428a
ranger163 <ahmed.nassar163@gmail.com>
2021-02-24 11:11:29 +0300

    Adding implementation to BaseActivity.kt

--- app/src/main/java/me/inassar/koindependencyinjection/base/BaseActivity.kt
+++ app/src/main/java/me/inassar/koindependencyinjection/base/BaseActivity.kt
@@ -1,4 +1,39 @@
 package me.inassar.koindependencyinjection.base
 
-class BaseActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<BINDING : ViewDataBinding, STATE : BaseState, VIEWMODEL : BaseViewModel>
+    (@LayoutRes val layoutId: Int) : AppCompatActivity() {
+
+    protected abstract val viewModel: VIEWMODEL
+    protected val state: STATE by lazy {
+        viewModel.provideState() as STATE
+    }
+    protected lateinit var binding: BINDING
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        initView()
+        initDataBinding()
+    }
+
+    open fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    open fun initDataBinding() {
+        DataBindingUtil.setContentView<BINDING>(this, layoutId)
+            .apply {
+                binding = this
+                lifecycleOwner = this@BaseActivity
+                executePendingBindings()
+            }
+    }
+
+    abstract fun observeViewState(state: STATE)
 }
