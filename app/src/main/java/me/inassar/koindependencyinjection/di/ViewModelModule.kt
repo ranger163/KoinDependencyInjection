7733f5087737f3d3de7f49e20dae2694afd20243
ranger163 <ahmed.nassar163@gmail.com>
2021-02-24 13:00:18 +0300

    Implementing ViewModelModule.kt file

--- app/src/main/java/me/inassar/koindependencyinjection/di/ViewModelModule.kt
+++ app/src/main/java/me/inassar/koindependencyinjection/di/ViewModelModule.kt
@@ -1,1 +1,6 @@
 package me.inassar.koindependencyinjection.di
+
+import me.inassar.koindependencyinjection.view.MainActivityViewModel
+import org.koin.dsl.module
+
+val viewModelModule = module { factory { MainActivityViewModel(get()) } }
