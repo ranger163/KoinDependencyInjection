79de07751f43b3b0be25f81b25109ee40f1f8a1f
ranger163 <ahmed.nassar163@gmail.com>
2021-02-24 15:39:11 +0300

    Adding full implementation to MainActivity.kt

--- app/src/main/java/me/inassar/koindependencyinjection/view/MainActivity.kt
+++ app/src/main/java/me/inassar/koindependencyinjection/view/MainActivity.kt
@@ -1,12 +1,42 @@
 package me.inassar.koindependencyinjection.view
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import me.inassar.koindependencyinjection.R
+import me.inassar.koindependencyinjection.base.BaseActivity
+import me.inassar.koindependencyinjection.databinding.MainActivityBinding
+import me.inassar.koindependencyinjection.snack
+import org.koin.android.ext.android.inject
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity :
+    BaseActivity<MainActivityBinding, MainActivityState, MainActivityViewModel>
+        (R.layout.activity_main) {
+
+    override val viewModel: MainActivityViewModel by inject()
+
+    override fun observeViewState(state: MainActivityState) {
+        // Observe the STATE here as this will handle API response
+        // API SUCCESS, FAILURE and LOADING
+
+        state.loginValidationState.observe(this, { loginState ->
+            when (loginState) {
+                is ValidateLoginState.UsernameEmpty -> {
+                    clearErrors()
+                    bindingViews().usernameEt.error = "Please enter username"
+                }
+                is ValidateLoginState.PasswordEmpty -> {
+                    clearErrors()
+                    bindingViews().passwordEt.error = "Please enter password"
+                }
+                is ValidateLoginState.Success -> {
+                    clearErrors()
+                    bindingViews().loginBtn.snack("Hooray, successful login")
+                }
+            }
+        })
     }
+
+    private fun clearErrors() {
+        bindingViews().usernameEt.error = null
+        bindingViews().passwordEt.error = null
+    }
+
 }
