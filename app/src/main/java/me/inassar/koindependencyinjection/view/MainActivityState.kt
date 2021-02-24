06c0e704cdafeea10568b3f008c186973bc95953
ranger163 <ahmed.nassar163@gmail.com>
2021-02-24 14:00:03 +0300

    Adding full implementation to MainActivityState.kt

--- app/src/main/java/me/inassar/koindependencyinjection/view/MainActivityState.kt
+++ app/src/main/java/me/inassar/koindependencyinjection/view/MainActivityState.kt
@@ -1,7 +1,17 @@
 package me.inassar.koindependencyinjection.view
 
+import androidx.lifecycle.MutableLiveData
 import me.inassar.koindependencyinjection.base.BaseState
 
 class MainActivityState : BaseState {
 
+    val username = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
+    val loginValidationState = MutableLiveData<ValidateLoginState>()
+
+}
+
+sealed class ValidateLoginState {
+    object EmailEmpty : ValidateLoginState()
+    object PasswordEmpty : ValidateLoginState()
 }
