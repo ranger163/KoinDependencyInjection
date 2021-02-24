9480af871cb76eaf96c25f45293278b37c9f65d5
ranger163 <ahmed.nassar163@gmail.com>
2021-02-24 11:26:32 +0300

    Moving MainActivity.kt to view package

--- app/src/main/java/me/inassar/koindependencyinjection/view/MainActivity.kt
+++ app/src/main/java/me/inassar/koindependencyinjection/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package me.inassar.koindependencyinjection
+package me.inassar.koindependencyinjection.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import me.inassar.koindependencyinjection.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
