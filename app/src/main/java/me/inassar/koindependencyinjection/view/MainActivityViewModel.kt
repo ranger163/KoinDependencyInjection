e086eb22d164a3773561e73e33ad6467ff68cf44
ranger163 <ahmed.nassar163@gmail.com>
2021-02-24 15:22:08 +0300

    Adding full implementation to MainActivityViewModel.kt

--- app/src/main/java/me/inassar/koindependencyinjection/view/MainActivityViewModel.kt
+++ app/src/main/java/me/inassar/koindependencyinjection/view/MainActivityViewModel.kt
@@ -3,6 +3,29 @@ package me.inassar.koindependencyinjection.view
 import me.inassar.koindependencyinjection.base.BaseState
 import me.inassar.koindependencyinjection.base.BaseViewModel
 
-class MainActivityViewModel(private val state: MainActivityState):BaseViewModel() {
-    override fun provideState(): BaseState =state
+class MainActivityViewModel(private val state: MainActivityState) : BaseViewModel() {
+    override fun provideState(): BaseState = state
+
+    fun login() {
+        if (!validUserInfo()) {
+            return
+        }
+    }
+
+    private fun validUserInfo(): Boolean {
+        return when {
+            state.username.value.isNullOrEmpty() -> {
+                state.loginValidationState.value = ValidateLoginState.UsernameEmpty
+                false
+            }
+            state.password.value.isNullOrEmpty() -> {
+                state.loginValidationState.value = ValidateLoginState.PasswordEmpty
+                false
+            }
+            else -> {
+                state.loginValidationState.value = ValidateLoginState.Success
+                true
+            }
+        }
+    }
 }
