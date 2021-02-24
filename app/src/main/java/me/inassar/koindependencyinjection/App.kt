3186a79f05e6c9eba0724dc1ac164a406b64f6e5
ranger163 <ahmed.nassar163@gmail.com>
2021-02-24 13:04:49 +0300

    Implementing Koin in App.kt application class

--- app/src/main/java/me/inassar/koindependencyinjection/App.kt
+++ app/src/main/java/me/inassar/koindependencyinjection/App.kt
@@ -1,4 +1,21 @@
 package me.inassar.koindependencyinjection
 
-class App {
+import android.app.Application
+import me.inassar.koindependencyinjection.di.stateModule
+import me.inassar.koindependencyinjection.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initKoin()
+    }
+
+    private fun initKoin() {
+        startKoin {
+            androidContext(applicationContext)
+            modules(listOf(stateModule, viewModelModule))
+        }
+    }
 }
