2f9d148fd12f0ff552691d0fe250bed86941749b
ranger163 <ahmed.nassar163@gmail.com>
2021-02-24 13:00:02 +0300

    Implementing StateModule.kt file

--- app/src/main/java/me/inassar/koindependencyinjection/di/StateModule.kt
+++ app/src/main/java/me/inassar/koindependencyinjection/di/StateModule.kt
@@ -1,2 +1,8 @@
 package me.inassar.koindependencyinjection.di
 
+import me.inassar.koindependencyinjection.view.MainActivityState
+import org.koin.dsl.module
+
+val stateModule = module {
+    factory { MainActivityState() }
+}
